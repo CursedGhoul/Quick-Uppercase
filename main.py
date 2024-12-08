9ebeec90294ddede77fd6c8ad79e634d1048c5c7
Ghoul <CursedGhoulBlox@gmail.com>
2024-12-08 16:42:08 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,2 @@
-string = input("Enter the whatever you would like to captilize everything in here: ").upper()
+string = input("Enter the whatever you would like to captilize everything in here: ").upper() # Quickly capitalize a string of letters with this thing I wrote
 print(string)
